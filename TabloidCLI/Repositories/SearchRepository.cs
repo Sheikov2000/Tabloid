c6a5b55194581cd126b2e81b7e32e1347f31097c
Nick Sheikov <sheikov2000@yahoo.com>
2023-06-19 12:02:58 -0400

    test

---
+++ TabloidCLI/Repositories/SearchRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using System.Threading.Tasks;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI
+{
+    public class SearchRepository : DatabaseConnector, IRepository<Search>
+    {
+        public SearchRepository(string connectionString) : base(connectionString) { }
+
+        public List<Search> GetAll()
+        {
+
+            using (SqlConnection conn = Connection)
+            {
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT id",
+                                        
+                }
+            }
+        }
